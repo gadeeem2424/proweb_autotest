420c06c81f0561577b35d8ac3ee328ab59ca747e
gadeeem2424 <almirauldaseva697@gmail.com>
2026-07-23 18:31:21 +0500

    test 20

--- conftest.py
+++ conftest.py
@@ -3,25 +3,10 @@ from selenium import webdriver
 
 @pytest.fixture
 def driver_chrome():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--window-size=1920,1080")
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome()
+    driver.maximize_window()
     driver.implicitly_wait(10)
     yield driver
     driver.quit()
 
-@pytest.fixture
-def driver_edge():
-    options = webdriver.EdgeOptions()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--window-size=1920,1080")
-    driver = webdriver.Edge(options=options)
-    driver.implicitly_wait(10)
-    yield driver
-    driver.quit()
 
