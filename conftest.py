7c0f20980946a66e1170c9c0a0529528ce6295c8
gadeeem2424 <almirauldaseva697@gmail.com>
2026-07-23 17:40:00 +0500

    new14

--- conftest.py
+++ conftest.py
@@ -3,7 +3,11 @@ from selenium import webdriver
 
 @pytest.fixture
 def driver_chrome():
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")  # Запуск без графического окна
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(options=options)
     driver.maximize_window()
     driver.implicitly_wait(10)
     yield driver
@@ -11,7 +15,11 @@ def driver_chrome():
 
 @pytest.fixture
 def driver_edge():
-    driver = webdriver.Edge()
+    options = webdriver.EdgeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Edge(options=options)
     driver.maximize_window()
     driver.implicitly_wait(10)
     yield driver
